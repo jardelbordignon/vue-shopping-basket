0d076f614cf168c73ae4f571d9a08cd1fe90157d
Jardel Bordignon <jardel1101@gmail.com>
2024-08-19 23:37:50 -0300

    :recycle: refactoring views structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Basket from '../views/ShoppingBasket.vue'
+import Home from '@/views/pages/Home.vue'
+import Basket from '@/views/pages/Basket.vue'
 
 const routes = [
   {
@@ -12,8 +12,7 @@ const routes = [
     path: '/basket',
     name: 'Basket',
     component: Basket
-  },
-  
+  }
 ]
 
 const router = createRouter({
